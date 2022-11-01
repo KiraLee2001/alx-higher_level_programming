6a453f0cc548c4bc44947f56c39de7948831e8a8
KiraLee2001 <olonisayeprecious@gmail.com>
2022-11-01 14:57:18 -0700

    create a database

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,3 @@
---Creates the database hbtn_0c_0 in my MYSQL server if it does not exist
+-- Creates the database hbtn_0c_0 in my MYSQL server if 
+-- it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
